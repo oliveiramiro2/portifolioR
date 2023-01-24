51558afd7a807f20ada012f3621f59400095441f
oliveiramiro2 <ramiromares95@gmail.com>
2023-01-24 03:33:13 -0300

    added tailwind config and start develop the page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div className='w-screen h-screen bg-black flex justify-center items-center'>
+        <p className='text-white'>HOME</p>
+    </div>
+  )
+}
+
+export default Home
